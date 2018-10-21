4f0c8fff0ec9580b8c7ddd6f3865e60c85fcad5b
Training project <koshelev198811@gmail.com>
2018-10-21 20:43:55 +0300

    Sid Bar

--- gulpfile.js
+++ gulpfile.js
@@ -1,140 +1,160 @@
-var gulp         = require('gulp'), // Подключаем Gulp
-    sass         = require('gulp-sass'), //Подключаем Sass пакет,
-    browserSync  = require('browser-sync'), // Подключаем Browser Sync
-    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-    cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
-    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    // pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
-    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
-    gcmq        = require('gulp-group-css-media-queries'),
-    smartgrid    = require('smart-grid');
+const gulp         = require('gulp'),
+       sass         = require('gulp-sass'),
+       gcmq         = require('gulp-group-css-media-queries'),
+	     autoprefixer = require('gulp-autoprefixer'),
+	     concat       = require('gulp-concat'),
+	     rename       = require('gulp-rename'),
+	     cleanCss     = require('gulp-clean-css'),
+	     uglifyjs     = require('gulp-uglifyjs'),
+	     pngquant     = require('imagemin-pngquant'),
+	     imagemin     = require('gulp-imagemin'),
+	     del          = require('del'),
+	     browserSync  = require('browser-sync').create(),
+	     smartGrid    = require('smart-grid');
 
+const cssFiles = [
+	'./src/libs/normalize.css/normalize.css',
+	'./src/css/style.css'
+      ],
+
+	    jsFiles = [
+	    	'./src/libs/jQuery/dist/jquery.js',
+	    	'./src/js/scripts.js',
+			];
 
 /* It's principal settings in smart grid project */
-var settings = {
-    outputStyle: 'scss', /* less || scss || sass || styl */
-    columns: 24, /* number of grid columns */
-    offset: '1rem', /* gutter width px || % || rem */
-    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
-    container: {
-        maxWidth: '119.2rem', /* max-width оn very large screen */
-        fields: '4rem' /* side fields */
-    },
-    breakPoints: {
-        lgXl: {
-            width: '1600px', /* -> @media (max-width: 1100px) */
+const settings = {
+	outputStyle: 'scss', /* less || scss || sass || styl */
+	columns: 12, /* number of grid columns */
+	offset: '1rem', /* gutter width px || % || rem */
+	mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+	container: {
+		maxWidth: '119.2rem', /* max-width оn very large screen */
+		fields: '4rem' /* side fields */
+	},
+	breakPoints: {
+		lg: {
+			width: '1100px', /* -> @media (max-width: 1100px) */
+		},
+		md: {
+			width: '960px'
+		},
+		sm: {
+			width: '780px',
+			fields: '15px' /* set fields only if you want to change container.fields */
+		},
+		xs: {
+			width: '560px'
+		}
+		/*
+		We can create any quantity of break points.
 
-        },
-        lg: {
-            width: '1200px', /* -> @media (max-width: 1100px) */
-					  // fields: '30px'
-        },
-        md: {
-            width: '960px'
-        },
-        sm: {
-            width: '780px',
-            // fields: '15px' /* set fields only if you want to change container.fields */
-        },
-        xs: {
-            width: '560px'
-        }
-        /* 
-        We can create any quantity of break points.
- 
-        some_name: {
-            width: 'Npx',
-            fields: 'N(px|%|rem)',
-            offset: 'N(px|%|rem)'
-        }
-        */
-    }
+		some_name: {
+				width: 'Npx',
+				fields: 'N(px|%|rem)',
+				offset: 'N(px|%|rem)'
+		}
+		*/
+	}
 };
 
-smartgrid('src/scss/params', settings);
+smartGrid('./src/scss/params/', settings);
 
+function scss() {
+	return gulp.src('./src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gcmq())
+		.pipe(gulp.dest('./src/css'))
+		.pipe(browserSync.stream())
+}
 
-gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('src/scss/**/*.scss') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(autoprefixer(['last 15 versions', '> 1%'], { cascade: true })) // Создаем префиксы
-        .pipe(gcmq())
-        .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
+function styles() {
+	return gulp.src(cssFiles)
+		.pipe(concat('main.css'))
+		.pipe(autoprefixer({
+			browsers: ['> 0.1%'],
+			cascade: false
+		}))
+		.pipe(gcmq())
+		.pipe(gulp.dest('./src/css'))
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'src' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
-    });
-});
+}
 
-gulp.task('scripts', function() {
-    return gulp.src([ // Берем все необходимые библиотеки
-        'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
-        'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
-        ])
-        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
-        .pipe(uglify()) // Сжимаем JS файл
-        .pipe(gulp.dest('src/js')); // Выгружаем в папку app/js
-});
+function stylesMin() {
+	return gulp.src('./src/css/main.css')
+		.pipe(cleanCss({level: 2}))
+		.pipe(rename({suffix: '.min' }))
+		.pipe(gulp.dest('./src/css'))
+		.pipe(browserSync.stream())
+}
+function scripts() {
+	return gulp.src(jsFiles)
+		.pipe(concat('libs.js'))
+		.pipe(gulp.dest('./src/js'))
+}
 
-gulp.task('css-libs', ['sass'], function() {
-    return gulp.src('scr/css/style.css') // Выбираем файл для минификации
-        .pipe(cssnano()) // Сжимаем
-        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-        .pipe(gulp.dest('src/css')); // Выгружаем в папку app/css
-});
+function scriptsMin() {
+	return gulp.src('./src/js/libs.js')
+		.pipe(uglifyjs({
+			toplevel: true
+		}))
+		.pipe(rename({suffix: '.min' }))
+		.pipe(gulp.dest('./src/js'))
+		.pipe(browserSync.stream())
+}
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: "./src/"
+		},
+		notify: false
+	});
+	gulp.watch('./src/*.html', browserSync.reload);
+	gulp.watch('./src/scss/**/*.scss', scss);
+	gulp.watch('./src/css/style.css', gulp.series(styles, stylesMin));
+	gulp.watch('./src/js/scripts.js', gulp.series(scripts, scriptsMin));
+	gulp.watch('./src/libs/**/*.js', gulp.series(scripts, scriptsMin));
+}
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('src/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
-    gulp.watch('src/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('src/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
-});
+function img() {
+	return gulp.src('./src/img/**/*')
+		.pipe(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('./dist/img'))
+}
 
-gulp.task('clean', function() {
-    return del.sync('dist'); // Удаляем папку dist перед сборкой
-});
+function distHtml() {
+	return gulp.src(['./src/*.html'])
+		.pipe(gulp.dest('dist'))
+}
 
-gulp.task('img', function() {
-    return gulp.src('src/img/**/*') // Берем все изображения из app
-        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            // use: [pngquant()]
-        })))
-        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
-});
+function distCss() {
+	return gulp.src(['./src/css/main.min.css'])
+		.pipe(gulp.dest('./dist/css'))
+}
 
-gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+function distJs() {
+	return gulp.src(['./src/js/libs.min.js'])
+		.pipe(gulp.dest('./dist/js'))
+}
 
-    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-        'src/css/*.css',
-        'src/css/libs.min.css'
-        ])
-    .pipe(gulp.dest('dist/css'))
+function distFonts() {
+	return gulp.src(['./src/fonts/**/*'])
+		.pipe(gulp.dest('./dist/fonts'))
+}
 
-    var buildFonts = gulp.src('src/fonts/**/*') // Переносим шрифты в продакшен
-    .pipe(gulp.dest('dist/fonts'))
+function clean() {
+	return del(['./dist/*'])
+}
 
-    var buildJs = gulp.src('src/js/**/*') // Переносим скрипты в продакшен
-    .pipe(gulp.dest('dist/js'))
+// gulp.task('scripts', scripts);
+// gulp.task('watch', watch);
+gulp.task('dist', gulp.series(clean, scss, styles, stylesMin, scripts, scriptsMin, distHtml, distCss, distJs,  distFonts),
+	                gulp.parallel(img));
 
-    var buildHtml = gulp.src('src/*.html') // Переносим HTML в продакшен
-    .pipe(gulp.dest('dist'));
+gulp.task('default', watch);
 
-});
-
-gulp.task('clear', function () {
-    return cache.clearAll();
-});
-
-gulp.task('default', ['watch']);
